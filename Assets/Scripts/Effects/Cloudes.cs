29ab09c68d3d8d8a51cc196f3edd69e41c0743ae
천민성 <albert6306@naver.com>
2025-09-30 14:53:45 +0900

    3차 빌드용 튜토리얼 텍스트 변경

--- Assets/Scripts/Effects/Cloudes.cs
+++ Assets/Scripts/Effects/Cloudes.cs
@@ -4,9 +4,26 @@ public class Cloudes : MonoBehaviour
 {
     public GameObject[] clouds;
     public TileManager tilemanager;
+    public float cloudSpeed;
+    private void Start()
+    {
+        foreach (var cloud in clouds) 
+        {
+            SetStartPosition(cloud);
+        }
+    }
+
     private void Update()
     {
+        foreach(var cloud in clouds)
+        {
+            cloud.transform.position += Vector3.right * cloudSpeed * Time.deltaTime;
 
+            if(cloud.transform.position.x >= 30f)
+            {
+                SetStartPosition(cloud);
+            }
+        }
     }
 
     public void OffAllClouds()
@@ -17,9 +34,9 @@ public class Cloudes : MonoBehaviour
         }
     }
 
-    public void SetPosition()
+    public void SetStartPosition(GameObject cloud)
     {
-        Vector3 position = new Vector3(-100f, 0f, Random.Range(tilemanager.DragAbleRect.y, tilemanager.DragAbleRect.w));
-
+        Vector3 position = new Vector3(Random.Range(-40f , -20f), 5f, Random.Range(tilemanager.DragAbleRect.y, tilemanager.DragAbleRect.w));
+        cloud.transform.position = position;
     }
 }
